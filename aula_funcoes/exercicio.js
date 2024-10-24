08eb999b58000c18784f3fe0dd20b86ff6e67b6b
renato-c-abreu <kynoxgamers@gmail.com>
2024-10-23 21:56:10 -0300

    Add files via upload

---
+++ aula_funcoes/exercicio.js
@@ -0,0 +1,24 @@
+function resultado_soma(a,b){
+    let total = a+b
+    console.log("Resultado da soma: ",total)
+}
+resultado_soma(20,10)
+
+function resultado_sub(c,d){
+    let total_sub = c-d
+    console.log("Resultado da subtração: ",total_sub)
+}
+resultado_sub(20,10)
+
+function resultado_div(e,f){
+    let total_div = e/f
+    console.log("Resultado da divisão: ",total_div)
+}
+resultado_div(20,10)
+
+
+function resultado_mult(g,h){
+    let total_mult = g*h
+    console.log("Resultado da multiplicação: ",total_mult)
+}
+resultado_mult(20,10)
